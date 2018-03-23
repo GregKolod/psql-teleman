be14c915c617c4c5fde6740202a356a3d68fc9fb
Wojciech Januszek <wojsamjan@gmail.com>
2018-03-23 20:34:54 +0100

    add TvGuide model

--- tvguide/models.py
+++ tvguide/models.py
@@ -1,3 +1,12 @@
+from django.contrib.postgres.fields import JSONField
 from django.db import models
 
-# Create your models here.
+
+class TvGuide(models.Model):
+    name = models.CharField(max_length=40)
+    date_at = models.DateTimeField
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
