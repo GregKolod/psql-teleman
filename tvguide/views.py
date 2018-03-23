8dd1cd4fcc16975f13c1565eb2c79a7797b77d75
Wojciech Januszek <wojsamjan@gmail.com>
2018-03-23 21:20:29 +0100

    add RetrieveUpdateDestroyChannel view

--- tvguide/views.py
+++ tvguide/views.py
@@ -26,3 +26,8 @@ class ListCreateChannel(generics.ListCreateAPIView):
     def perform_create(self, serializer):
         tvguide = get_object_or_404(models.TvGuide, pk=self.kwargs.get('tvguide_pk'))
         serializer.save(tvguide=tvguide)
+
+
+class RetrieveUpdateDestroyChannel(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.Channel.objects.all()
+    serializer_class = serializers.ChannelSerializer
