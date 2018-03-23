179a2178a386bdaed4705d0745c3305127a8863e
Wojciech Januszek <wojsamjan@gmail.com>
2018-03-23 19:05:51 +0100

    change database to PostgreSQL

--- psqlteleman/settings.py
+++ psqlteleman/settings.py
@@ -75,8 +75,14 @@ WSGI_APPLICATION = 'psqlteleman.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'teleman',
+        'USER': 'teleman',
+        'PASSWORD': 'psqlteleman',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
