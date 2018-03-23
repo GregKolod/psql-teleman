de09bdcd06607940b11048fefd792c7402cec225
Wojciech Januszek <wojsamjan@gmail.com>
2018-03-23 21:02:04 +0100

    add ListCreateTvGuide view

--- tvguide/views.py
+++ tvguide/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+from rest_framework import generics
+
+from . import models
+from . import serializers
+
+
+class ListCreateTvGuide(generics.ListCreateAPIView):
+    queryset = models.TvGuide.objects.all()
+    serializer_class = serializers.TvGuideSerializer
