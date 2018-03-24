7339c7a0ab30ce47dd8a9e910e5e08761837c107
Wojciech Januszek <wojsamjan@gmail.com>
2018-03-24 01:36:16 +0100

    define project's urls

--- psqlteleman/urls.py
+++ psqlteleman/urls.py
@@ -20,4 +20,8 @@ from tvguide import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^api-auth/', include('rest_framework.urls',
+                               namespace='rest_framework')),
+    url(r'^api/v1/tvguides/', include('tvguide.urls',
+                                      namespace='tvguides')),
 ]
