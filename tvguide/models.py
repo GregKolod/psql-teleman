a7c88d9952a6625110dc90b4184565661ad25a4c
Wojciech Januszek <wojsamjan@gmail.com>
2018-03-25 12:19:00 +0200

    update Channel model

--- tvguide/models.py
+++ tvguide/models.py
@@ -15,7 +15,7 @@ class TvGuide(models.Model):
 class Channel(models.Model):
     tvguide = models.ForeignKey(TvGuide, related_name='channels', on_delete=models.CASCADE)
     name = models.CharField(max_length=40)
-    # daily_channel = JSONField()
+    daily_channel = JSONField(blank=True, null=True, default=None)
 
     def __str__(self):
         return self.name
