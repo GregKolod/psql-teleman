777498a70eae2a78f1a09d61d91bee05eec1d97a
Wojciech Januszek <wojsamjan@gmail.com>
2018-03-25 15:05:13 +0200

    add unique to Channel model, overwrite its save method

--- tvguide/models.py
+++ tvguide/models.py
@@ -1,6 +1,8 @@
 from django.contrib.postgres.fields import JSONField
 from django.db import models
 
+from . import scrapers
+
 
 class TvGuide(models.Model):
     name = models.CharField(max_length=40)
@@ -16,7 +18,19 @@ class Channel(models.Model):
     tvguide = models.ForeignKey(TvGuide, related_name='channels', on_delete=models.CASCADE)
     name = models.CharField(max_length=40)
     quote_page = models.CharField(max_length=255, default='')
-    daily_channel = JSONField(blank=True, null=True, default={})
+    daily_channel = JSONField(blank=True, default='')
+
+    class Meta:
+        unique_together = ['name', 'tvguide']
 
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.daily_channel:
+            msg = 'Scraping: {}'.format(self.quote_page)
+            print(msg)
+
+            channel_list = scrapers.channel_scrapper(self.quote_page)
+            self.daily_channel = {"channel_list": channel_list}
+            super(Channel, self).save(*args, **kwargs)
